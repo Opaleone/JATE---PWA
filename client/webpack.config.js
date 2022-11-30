31728023c9d35e463c1658bbeb97ac6a2eb2c6fa
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-11-30 17:48:36 -0600

    Added manifest creation to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -28,6 +28,22 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'service-worker.js',
       }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('src', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
