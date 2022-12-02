1955c87d8acec3ab23d8784226be10a62c1fc3c4
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-02 06:24:21 -0600

    Removed irrelevant comments and added dist folder to main brach for heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,7 +27,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate(pageCache)
